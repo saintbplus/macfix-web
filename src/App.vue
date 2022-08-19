72b75bd8c54ab3aef9e2d2d556ca9500da6a7d11
dev.thanapon@gmail.com <dev.thanapon@gmail.com>
2022-08-20 01:36:45 +0700

    modify base app and style
    delete unuse style and add some global.
    - a tag to color like normal text and underline only on hover
    - ul tag use no list style and no padding.
    
    App.vue style wrapper and header constrain width size.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <script setup>
-import { RouterView } from "vue-router";
+import { RouterLink, RouterView } from "vue-router";
+import HeaderNavigator from "@/components/HeaderNavigator/HeaderNavigator.vue";
 </script>
 
 <template>
+  <header>
+    <div class="header-inner">
+      <div class="icon">
+        <RouterLink to="/feed">App Icon</RouterLink>
+      </div>
+      <HeaderNavigator />
+    </div>
+  </header>
   <div class="wrapper">
-    <h1>app</h1>
     <RouterView />
   </div>
 </template>
 
 <style scoped>
+header {
+  background-color: grey;
+  height: 93px;
+  margin-bottom: 50px;
+}
+
+.header-inner,
+.wrapper {
+  height: 93px;
+  max-width: 1444px;
+  margin: 0 auto;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+
+.header-inner {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 .wrapper {
   text-align: center;
-}
-h1 {
-  color: red;
 }
 </style>
