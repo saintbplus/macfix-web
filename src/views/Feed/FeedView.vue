73302a00bd05bcbe53e187a5f6a58f5e52e20d5e
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-08-21 12:59:25 +0700

    add skeload

--- src/views/Feed/FeedView.vue
+++ src/views/Feed/FeedView.vue
@@ -2,15 +2,22 @@
 import { useNavStore, pagesEnum } from "@/stores/navStore";
 import StatusBar from "@/components/StatusReporter/StatusBar.vue";
 import { onMounted, reactive } from "vue";
+import StatusReportLoad from "@/components/SkeletonLoad/StatusReportLoad.vue";
 
 // mock report data from server
-const reports = reactive({
-  repairJob: 0,
-  jobPending: 1,
-  history: 8,
+const state = reactive({
+  isLoading: true,
+  reports: { repairJob: 0, jobPending: 1, history: 8 },
 });
 
+const mockLoad = (timeMs) => {
+  setTimeout(() => {
+    state.isLoading = false;
+  }, timeMs);
+};
+
 onMounted(() => {
+  mockLoad(2000);
   const { changeCurrent } = useNavStore();
   // change page state on nav
   changeCurrent(pagesEnum.other);
@@ -19,25 +26,34 @@ onMounted(() => {
 
 <template>
   <main>
-    <div class="user">
-      <div class="user-greeting">
-        <p>
-          <span>สวัสดี,</span>
-          <span class="username">user</span>
-        </p>
+    <StatusReportLoad v-if="state.isLoading" />
+    <div class="feed-wrapper" v-else>
+      <div class="user">
+        <div class="user-greeting">
+          <p>
+            <span>สวัสดี,</span>
+            <span class="username">user</span>
+          </p>
+        </div>
+        <div class="user-menu">
+          <button>User</button>
+        </div>
       </div>
-      <div class="user-menu">
-        <button>User</button>
+      <StatusBar
+        title="รายการรอส่งซ่อม"
+        :status="state.reports.jobPending + ' งาน'"
+      />
+      <StatusBar
+        title="สถานะการซ่อม"
+        :status="state.reports.repairJob + ' งาน'"
+      />
+      <StatusBar
+        title="ข้อมูลการส่งซ่อมในอดีต"
+        :status="state.reports.history + ' งาน'"
+      />
+      <div class="bottom">
+        <button>คุยกับช่าง</button>
       </div>
-    </div>
-    <StatusBar title="รายการรอส่งซ่อม" :status="reports.jobPending + ' งาน'" />
-    <StatusBar title="สถานะการซ่อม" :status="reports.repairJob + ' งาน'" />
-    <StatusBar
-      title="ข้อมูลการส่งซ่อมในอดีต"
-      :status="reports.history + ' งาน'"
-    />
-    <div class="bottom">
-      <button>คุยกับช่าง</button>
     </div>
   </main>
 </template>
@@ -48,11 +64,11 @@ onMounted(() => {
   justify-content: space-between;
 }
 
-main {
+.feed-wrapper {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 530px;
+  height: 430px;
 }
 
 .bottom {
