77e5abc04b202e2fa2627aea1532060b86be13ba
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-08-21 12:59:51 +0700

    base layout

--- src/views/PendingJob/PendingJobView.vue
+++ src/views/PendingJob/PendingJobView.vue
@@ -1,8 +1,28 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, reactive } from "vue";
 import { useNavStore, pagesEnum } from "@/stores/navStore";
+import ListSkeleLoad from "@/components/SkeletonLoad/ListSkeleLoad.vue";
+import WorkList from "@/components/WorkList/WorkList.vue";
+import { jobsData } from "./data";
+
+const state = reactive({
+  pendingList: [],
+  isLoading: true,
+});
+
+const mockLoad = (timeMs) => {
+  setTimeout(() => {
+    state.pendingList = jobsData.map((job) => ({
+      id: job.id,
+      title: job.device,
+      date: job.requestDate,
+    }));
+    state.isLoading = false;
+  }, timeMs);
+};
 
 onMounted(() => {
+  mockLoad(2000);
   // change page state on nav.
   const { changeCurrent } = useNavStore();
   changeCurrent(pagesEnum.pendingList);
@@ -10,7 +30,24 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>Pending Job View</div>
+  <div>
+    <h1 class="page-title">รายการรอส่งซ่อม</h1>
+    <ListSkeleLoad v-if="state.isLoading" />
+    <div v-else>
+      <div class="list-info">
+        <p class="list-title">รายการ</p>
+      </div>
+      <WorkList :items="state.pendingList" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.page-title {
+  margin-bottom: 1rem;
+}
+.list-info {
+  display: flex;
+  justify-content: flex-start;
+}
+</style>
---
+++ src/views/PendingJob/data.js
@@ -0,0 +1,26 @@
+export const jobsData = [
+  {
+    id: "22b",
+    device: "ipad pro",
+    detail: "Change change port",
+    cost: 4500,
+    requestDate: new Date().getDate(),
+    status: "pending",
+  },
+  {
+    id: "42xb",
+    device: "ipad pro",
+    detail: "Change screen",
+    cost: 3200,
+    requestDate: new Date().getDate(),
+    status: "pending",
+  },
+  {
+    id: "58gd",
+    device: "ipad pro",
+    detail: "Change Battery",
+    cost: 3500,
+    requestDate: new Date().getDate(),
+    status: "pending",
+  },
+];
